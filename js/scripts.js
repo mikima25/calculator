25f0b70b8081a506bfb5d014fbd237184925f8f0
mikima25 <vtalism20@gmail.com>
2017-02-15 10:44:41 +0300

    cacalculator added

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+
+var sub = function(number1, number2) {
+return number1 - number2;
+};
+var number1 = parseInt(prompt("Entera number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+alert(sub(number1, number2));
+
+
+var divide = function(number1, number2) {
+return number1 / number2;
+};
+var number1 = parseInt(prompt("Entera number: "));
+var number2 = parseInt(prompt("Enter another number: "));
+alert(divide(number1, number2));
